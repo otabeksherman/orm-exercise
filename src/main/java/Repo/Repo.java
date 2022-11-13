31f2f8cda7166894022a62193a9b08ce75146588
khader-zatari <khaderzatari@gmail.com>
2022-11-13 19:26:59 +0200

    start think

---
+++ src/main/java/Repo/Repo.java
@@ -0,0 +1,32 @@
+package Repo;
+
+public class Repo {
+    public Repo() {
+    }
+    //Read
+
+    //read all item from the table
+    //get one item by id
+    //Get item(s) by any property
+
+    //Add
+
+    //insert one item to table
+    //insert multiple items to table
+    //update//
+    //update one property for item by an id
+    //update entire item//
+
+    //Delete
+
+    //delete Single item deletion by any property (delete user with email x)
+    //delete Multiple item deletion by any property (delete all users called x)
+    //Delete entire table (truncate)
+
+
+    //Table Creation
+
+    //the rest
+
+
+}
